eadd6109312f83e2cc4de2794dc9b3ace017af51
manalibhujbalcmaug25-cloud <manali.bhujbal.cmaug25@gmail.com>
2026-01-02 10:45:54 +0530

    Add files via upload

---
+++ ECanopy/Models/House.cs
@@ -0,0 +1,7 @@
+
+public class House {
+    public int HouseId { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public string Type { get; set; }
+}
---
+++ ECanopy/Models/Item.cs
@@ -0,0 +1,6 @@
+
+public class Item {
+    public int ItemId { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+}
